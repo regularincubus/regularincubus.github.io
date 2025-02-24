a18d500fc984cd7b69de2bb5f3f9d19cbba41116
regularincubus <sawi131202@gmail.com>
2025-02-24 09:56:18 +0100

    base for new calc

--- calcs.js
+++ calcs.js
@@ -1,12 +1,21 @@
-function calculateDMG()
-let scalingvalue = parseFloat(document.getElementById("scalingvalue").value); /*ATK/DEF/HP/WHATEVER*/
-let ability = parseFloat(document.getElementById("ability").value) / 100;
-let addDMG = parseFloat(document.getElementById("addDMG").value)
-let catalyze = parseFloat(document.getElementById("catalyze").value)
+function calculateDMG() {
+    const scalingvalue = parseFloat(document.getElementById("scalingvalue").value); /*ATK/DEF/HP/WHATEVER*/
+    const ability = parseFloat(document.getElementById("ability").value) / 100;
+    const addDMG = parseFloat(document.getElementById("addDMG").value)
 
-let damage = (scalingvalue * ability + addDMG + catalyze) * DMGbonus * DEFmulti * RESmulti * AMPmulti
+let damage = (scalingvalue * ability + addDMG) * DMGbonus * DEFmulti * RESmulti * AMPmulti
 
-function calculateAddDMG
-let adddmgscaling = parseFloat(document.getElementById("adddmgscaling").value)
-let adddmgvalue = parseFloat(document.getElementById("adddmgvalue").value)
-let damage = adddmgscaling * adddmgvalue / 100
+    const reacbonus = parseFloat(document.getElementById("reacbonus").value) / 100;
+    const em = parseFloat(document.getElementById("em").value);
+    const embonus = 2.78 * ((em/(em+1400)) * 1
+
+ if (document.getElementById("melt").checked) {
+     result = 2 * (1 + (embonus / 100) + reacbonus);
+        } else if (document.getElementById("revmelt").checked) {
+    result = 1.5 * (1 + (embonus / 100) + reacbonus);
+    } else if (document.getElementById("vape").checked) {
+    result = 2 * (1 + (embonus / 100) + reacbonus);
+    } else if (document.getElementById("revvape").checked) {
+        result = 1.5 * (1 + (embonus / 100) + reacbonus);
+    }
+}
